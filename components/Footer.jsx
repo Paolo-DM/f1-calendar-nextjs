b9ced849afc6a67b7c47e49be9a555a5c8fad2ce
Paolo Di Martino <paolo.dimartino@outlook.it>
2022-11-03 17:03:30 +0100

    Changed logo img, added description and changed text color

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,7 +1,46 @@
-import React from "react";
+import { Card, Grid, Text, Link } from "@nextui-org/react";
 
-function Footer() {
-  return <div>Footer</div>;
+export default function Footer() {
+  return (
+    <Card
+      css={{ p: "$6", mw: "100%", backgroundColor: "#0b2834", border: "none" }}
+    >
+      <Card.Header>
+        <img alt="nextui logo" src="./f1_logo.svg" width="84px" height="68px" />
+        <Grid.Container css={{ pl: "$6" }}>
+          <Grid xs={12}>
+            <Text h4 css={{ lineHeight: "$xs", color: "white" }}>
+              F1Calendar
+            </Text>
+          </Grid>
+          <Grid xs={12}>
+            <Link
+              css={{ color: "$accents8" }}
+              icon
+              href="www.f1-calendar-paolo-dm.vercel.app"
+              target="_blank"
+            >
+              www.f1-calendar-paolo-dm.vercel.app
+            </Link>
+          </Grid>
+        </Grid.Container>
+      </Card.Header>
+      <Card.Body css={{ py: "$2" }}>
+        <Text css={{ color: "white" }}>
+          A web app for Formula 1 fans to learn about standings and schedules of
+          current or past F1 seasons. Made by Paolo Di Martino
+        </Text>
+      </Card.Body>
+      <Card.Footer>
+        <Link
+          icon
+          color="primary"
+          target="_blank"
+          href="https://github.com/nextui-org/nextui"
+        >
+          Visit source code on GitHub.
+        </Link>
+      </Card.Footer>
+    </Card>
+  );
 }
-
-export default Footer;
