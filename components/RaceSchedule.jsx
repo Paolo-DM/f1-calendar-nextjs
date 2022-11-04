29e01d4f5c1955ce473c8c9ba450a69e8b84fc54
Paolo Di Martino <paolo.dimartino@outlook.it>
2022-11-04 16:56:37 +0100

    Changed dates to UTC to local times, added qualifying and sprint race schedules, changed font to mono and font size depenging on the screen width

--- components/RaceSchedule.jsx
+++ components/RaceSchedule.jsx
@@ -1,23 +1,58 @@
 import { Button } from "@nextui-org/react";
-import Link from "next/link";
 
 function RaceSchedule({ data, raceId }) {
+  const raceDate = new Date(
+    data.RaceTable.Races[raceId].date + " " + data.RaceTable.Races[raceId].time
+  );
+  const qualifyingDate = new Date(
+    data.RaceTable.Races[raceId].Qualifying.date +
+      " " +
+      data.RaceTable.Races[raceId].Qualifying.time
+  );
+  const sprintDate = data.RaceTable.Races[raceId].Sprint
+    ? new Date(
+        data.RaceTable.Races[raceId].Sprint?.date +
+          " " +
+          data.RaceTable.Races[raceId].Sprint?.time
+      )
+    : null;
+
   return (
     <div className=" h-full flex items-center justify-center py-5 border-gray-400 border-t  md:border-none md:py-0">
-      <div className="flex flex-col gap-4 ">
-        <h1 className="text-3xl">{data.RaceTable.Races[raceId].raceName}</h1>
-        <p className="text-xl">
+      <div className="flex flex-col gap-4 font-mono md:font-sans lg:font-mono">
+        <h1 className="text-3xl lg:text-4xl">
+          {data.RaceTable.Races[raceId].raceName}
+        </h1>
+        <p className="text-xl lg:text-2xl ">
           <strong>Circuit</strong>:{" "}
-          {data.RaceTable.Races[raceId].Circuit.circuitName}
+          <span className="text-gray-600">
+            {data.RaceTable.Races[raceId].Circuit.circuitName}
+          </span>
         </p>
-        <p className="text-xl">
-          <strong>Date</strong>: {data.RaceTable.Races[raceId].date}
+        <p className="text-xl lg:text-2xl ">
+          <strong>Qualifying</strong>:{" "}
+          <span className="text-gray-600">
+            {qualifyingDate.toLocaleString().slice(0, -3)}
+          </span>
         </p>
-        <p className="text-xl">
-          <strong>Time</strong>: {data.RaceTable.Races[raceId].time}
+        <p className="text-xl lg:text-2xl ">
+          <strong>Sprint Race</strong>:{" "}
+          {sprintDate ? (
+            <span className="text-gray-600">
+              {sprintDate.toLocaleString().slice(0, -3)}
+            </span>
+          ) : (
+            <span className="text-gray-600">{"n/a"}</span>
+          )}
+        </p>
+        <p className="text-xl lg:text-2xl ">
+          <strong>Race</strong>:{" "}
+          <span className="text-gray-600">
+            {raceDate.toLocaleString().slice(0, -3)}
+          </span>
         </p>
 
-        <Button bordered auto className="w-1/2">
+        <Button bordered auto className="w-1/2  text-lg lg:text-xl">
           <a
             href={`${data.RaceTable.Races[raceId].Circuit.url}`}
             target="_blank"
