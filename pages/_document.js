9956ebd6ac7bb3147477561724f5e30a55a7406d
Paolo Di Martino <paolo.dimartino@outlook.it>
2022-11-06 15:54:08 +0100

    Added Raleway font

--- pages/_document.js
+++ pages/_document.js
@@ -14,7 +14,15 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head>{CssBaseline.flush()}</Head>
+        <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Raleway:wght@300&display=swap"
+            rel="stylesheet"
+          />
+          {CssBaseline.flush()}
+        </Head>
         <body>
           <Main />
           <NextScript />
