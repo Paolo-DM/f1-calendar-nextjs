7b88c95100459aefd3b001fa295c62942c20edae
Paolo Di Martino <paolo.dimartino@outlook.it>
2022-11-08 15:30:26 +0100

    Added fetch for yearlyResults and passed them as props

--- pages/calendar/index.js
+++ pages/calendar/index.js
@@ -8,18 +8,28 @@ export const getStaticProps = async () => {
   const res = await fetch(`https://ergast.com/api/f1/${year}.json`);
   const schedule = await res.json();
 
+  const res1 = await fetch(
+    `https://ergast.com/api/f1/${year}/results.json?limit=400`
+  );
+  const results = await res1.json();
+
   return {
     props: {
       schedule: schedule,
+      yearlyResults: results,
     },
   };
 };
 
-function Calendar({ schedule }) {
+function Calendar({ schedule, yearlyResults }) {
   return (
     <>
       <YearPicker></YearPicker>
-      <FullCalendar schedule={schedule} type="Driver"></FullCalendar>
+      <FullCalendar
+        schedule={schedule}
+        yearlyResults={yearlyResults}
+        type="Driver"
+      ></FullCalendar>
     </>
   );
 }
