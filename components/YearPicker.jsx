1eb9e796db2d9c049be5e5e42d62ae20cfeef916
Paolo Di Martino <paolo.dimartino@outlook.it>
2022-11-09 19:05:05 +0100

    Changed appearance by usying a Collapse item to show years, used state Hook to change the year to the selected one

--- components/YearPicker.jsx
+++ components/YearPicker.jsx
@@ -1,19 +1,42 @@
-import React from "react";
-import {
-  ChevronRightIcon,
-  ChevronLeftIcon,
-  ChevronDoubleLeftIcon,
-  ChevronDoubleRightIcon,
-} from "@heroicons/react/solid";
+import { Collapse } from "@nextui-org/react";
+import Link from "next/link";
+import { forwardRef } from "react";
 
-function YearPicker() {
+function YearPicker({ year, setYear, currentYear }) {
+  const years = [];
+  for (let i = 1950; i <= currentYear; i++) {
+    years.push(i);
+  }
+
   return (
-    <div className="flex justify-evenly items-center w-1/3 mx-auto py-4">
-      <ChevronDoubleLeftIcon className="w-10" />
-      <ChevronLeftIcon className="w-10" />
-      <h1 className="text-3xl font-['Raleway'] font-semibold">2022</h1>
-      <ChevronRightIcon className="w-10" />
-      <ChevronDoubleRightIcon className="w-10" />
+    <div className="md:w-[50%] md:mx-auto my-3 mx-1">
+      <Collapse
+        className=""
+        bordered
+        title={
+          <p className="text-2xl font-['Raleway'] font-semibold">{year}</p>
+        }
+        subtitle={
+          <p className="text-xl font-['Raleway']">
+            Choose a year to see its calendar and results
+          </p>
+        }
+      >
+        <div className="flex flex-wrap gap-4">
+          {years.map((year) => {
+            return (
+              <Link key={year} href={`/calendar/${year}`}>
+                <a
+                  className="text-[#0072f5] hover:text-blue-400"
+                  onClick={() => setYear(year)}
+                >
+                  {year}
+                </a>
+              </Link>
+            );
+          })}
+        </div>
+      </Collapse>
     </div>
   );
 }
