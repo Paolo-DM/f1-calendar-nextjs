3fe9f3206e4c054a594924ff2f873e899f9edc9e
Paolo Di Martino <paolo.dimartino@outlook.it>
2022-11-09 19:09:13 +0100

    Set current year as the default one when opening the calendar page

--- pages/calendar/index.js
+++ pages/calendar/index.js
@@ -1,15 +1,14 @@
-import React from "react";
+import { useState } from "react";
 import YearPicker from "../../components/YearPicker";
 import FullCalendar from "../../components/FullCalendar";
 
-const year = 2022;
-
+const currentYear = new Date().getFullYear();
 export const getStaticProps = async () => {
-  const res = await fetch(`https://ergast.com/api/f1/${year}.json`);
+  const res = await fetch(`https://ergast.com/api/f1/${currentYear}.json`);
   const schedule = await res.json();
 
   const res1 = await fetch(
-    `https://ergast.com/api/f1/${year}/results.json?limit=400`
+    `https://ergast.com/api/f1/${currentYear}/results.json?limit=400`
   );
   const results = await res1.json();
 
@@ -22,13 +21,18 @@ export const getStaticProps = async () => {
 };
 
 function Calendar({ schedule, yearlyResults }) {
+  const [year, setYear] = useState(currentYear);
   return (
     <>
-      <YearPicker></YearPicker>
+      <YearPicker
+        year={year}
+        setYear={setYear}
+        currentYear={currentYear}
+      ></YearPicker>
       <FullCalendar
         schedule={schedule}
         yearlyResults={yearlyResults}
-        type="Driver"
+        year={year}
       ></FullCalendar>
     </>
   );
