76a4752f90a5f8781fee63b0364fdb8d83a9cdb1
Paolo Di Martino <paolo.dimartino@outlook.it>
2022-11-12 12:32:05 +0100

    Created routes to standings pages

---
+++ pages/standings/driver-standings/[year].js
@@ -0,0 +1,42 @@
+import { useState } from "react";
+import YearPicker from "../../../components/YearPicker";
+import FullCalendar from "../../../components/FullCalendar";
+import { yearList } from "../../../components/YearPicker";
+const currentYear = new Date().getFullYear();
+
+export async function getStaticPaths() {
+  // Get the paths we want to pre-render based on years
+  const paths = yearList(currentYear).map((y) => ({
+    params: { year: y.toString() },
+  }));
+  // We'll pre-render only these paths at build time.
+  return { paths, fallback: false };
+}
+
+export const getStaticProps = async ({ params }) => {
+  const res = await fetch(`https://ergast.com/api/f1/${params.year}.json`);
+  const schedule = await res.json();
+
+  return {
+    props: {
+      schedule: schedule,
+      selectedYear: params.year,
+    },
+  };
+};
+
+function CalendarDetail({ schedule, selectedYear }) {
+  const [year, setYear] = useState(selectedYear);
+  return (
+    <>
+      <YearPicker
+        year={year}
+        setYear={setYear}
+        currentYear={currentYear}
+      ></YearPicker>
+      <FullCalendar schedule={schedule} year={year}></FullCalendar>
+    </>
+  );
+}
+
+export default CalendarDetail;
---
+++ pages/standings/index.js
@@ -0,0 +1,10 @@
+import StandingsChooser from "../../components/StandingsChooser";
+import Link from "next/link";
+
+export default function Standings() {
+  return (
+    <>
+      <StandingsChooser></StandingsChooser>
+    </>
+  );
+}
