3c2226f0f76f142bedc1df72c0cb42112bc64255
Paolo Di Martino <paolo.dimartino@outlook.it>
2022-11-12 18:08:57 +0100

    Added conditional rendering of links depending on the current path

--- components/YearPicker.jsx
+++ components/YearPicker.jsx
@@ -9,7 +9,7 @@ export const yearList = (currY) => {
   return years;
 };
 
-function YearPicker({ year, setYear, currentYear }) {
+function YearPicker({ year, setYear, currentYear, route }) {
   return (
     <div className="md:w-[50%] md:mx-auto my-3 mx-1">
       <Collapse
@@ -25,16 +25,43 @@ function YearPicker({ year, setYear, currentYear }) {
       >
         <div className="flex flex-wrap gap-4">
           {yearList(currentYear).map((year) => {
-            return (
-              <Link key={year} href={`/calendar/${year}`}>
-                <a
-                  className="text-[#0072f5] hover:text-blue-400"
-                  onClick={() => setYear(year)}
+            if (route === "calendar") {
+              return (
+                <Link key={year} href={`/calendar/${year}`}>
+                  <a
+                    className="text-[#0072f5] hover:text-blue-400"
+                    onClick={() => setYear(year)}
+                  >
+                    {year}
+                  </a>
+                </Link>
+              );
+            } else if (route === "driver-standings") {
+              return (
+                <Link key={year} href={`/standings/driver-standings/${year}`}>
+                  <a
+                    className="text-[#0072f5] hover:text-blue-400"
+                    onClick={() => setYear(year)}
+                  >
+                    {year}
+                  </a>
+                </Link>
+              );
+            } else {
+              return (
+                <Link
+                  key={year}
+                  href={`/standings/constructor-standings/${year}`}
                 >
-                  {year}
-                </a>
-              </Link>
-            );
+                  <a
+                    className="text-[#0072f5] hover:text-blue-400"
+                    onClick={() => setYear(year)}
+                  >
+                    {year}
+                  </a>
+                </Link>
+              );
+            }
           })}
         </div>
       </Collapse>
