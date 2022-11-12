7b68d9d238a92247f32c8029b5ad3db5f523197a
Paolo Di Martino <paolo.dimartino@outlook.it>
2022-11-12 09:43:00 +0100

    Reduced eye icon width on small screens, not using anymore yearlyResults as props

--- components/ScrollModal.jsx
+++ components/ScrollModal.jsx
@@ -1,14 +1,14 @@
-import { Modal, useModal, Button, Text } from "@nextui-org/react";
+import { Modal, useModal, Button } from "@nextui-org/react";
 import { EyeIcon } from "@heroicons/react/outline";
 import RaceStanding from "./RaceStanding";
 
-export default function ScrollModal({ yearResults, raceRound, year }) {
+export default function ScrollModal({ raceRound, year }) {
   const { setVisible, bindings } = useModal();
   return (
     <div>
       <EyeIcon
         onClick={() => setVisible(true)}
-        className="w-8 hover:text-[#979797a9] hover:cursor-pointer text-[#979797]"
+        className="w-6 md:w-8 hover:text-[#979797a9] hover:cursor-pointer text-[#979797]"
       ></EyeIcon>
       <Modal
         scroll
