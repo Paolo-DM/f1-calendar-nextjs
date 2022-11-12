1cc48c8f6e3a52b159c923033f73896ed893e2a4
Paolo Di Martino <paolo.dimartino@outlook.it>
2022-11-12 09:17:31 +0100

    Modal skeleton imported from NextUI, on eye icon click the modal opens

---
+++ components/ScrollModal.jsx
@@ -0,0 +1,39 @@
+import { Modal, useModal, Button, Text } from "@nextui-org/react";
+import { EyeIcon } from "@heroicons/react/outline";
+import RaceStanding from "./RaceStanding";
+
+export default function ScrollModal({ yearResults, raceRound, year }) {
+  const { setVisible, bindings } = useModal();
+  return (
+    <div>
+      <EyeIcon
+        onClick={() => setVisible(true)}
+        className="w-8 hover:text-[#979797a9] hover:cursor-pointer text-[#979797]"
+      ></EyeIcon>
+      <Modal
+        scroll
+        blur
+        className=""
+        width="900px"
+        aria-labelledby="modal-title"
+        aria-describedby="modal-description"
+        {...bindings}
+      >
+        <Modal.Body>
+          <RaceStanding year={year} raceRound={raceRound} />
+        </Modal.Body>
+        <Modal.Footer>
+          <Button
+            auto
+            flat
+            bordered
+            color="error"
+            onClick={() => setVisible(false)}
+          >
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </div>
+  );
+}
